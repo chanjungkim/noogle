0edc7ae77d10b07732404c1baaa7828d6cdcb253
evan <chanjungskim@gmail.com>
2023-05-28 00:40:58 +0900

    Update: migrate to mv3 & handling new Naver design

--- core.js
+++ core.js
@@ -1,40 +1,8 @@
-$(function () {
-    $('#NM_TOP_BANNER').remove();
-    $('.column_left').remove();
-    $('#NM_scroll_top_btn').remove();
-    $('#u_skip').remove();
-    $('#timesquare').remove();
-    $('#veta_branding').remove();
-    $('#shopcast').remove();
-    $('.banner_area').remove();
-    $('.notice_area').remove();
-    $('#gnb').remove();
-    $('.service_area').remove();
-    $('#footer').remove();
-    // $('.container').remove();
-    /*
-    $('.aside').remove();
-    $('.notice').remove();
-    $('.column_left').remove();
-    $('.aside').remove();
-    $('.notice').remove();
-    $('.area_links').remove();
-    $('.column_bottom').remove();
-    $('.column_banner').remove();
-    // $('.naver_logo').remove();
+window.onload = function() {
+    console.log('Hello World!!');
 
-    $(".u_skip").before($('.section_minime, #account'));
-    $(".u_skip").before("<div id='noogle-container' style='width:100%; height:30%;display: inline-grid;'></div>");
-    $("#noogle-container").append($('.area_logo'));
-    $("#noogle-container").append("<div></div>");
-    $('.area_logo').css('height', 'auto');
-    $('.area_logo').after($('#search'));
-    $('.area_logo, #search').css('width', '50%');
-    $('.area_logo, #search').css('margin', 'auto');
-    */
-    // $('.container').remove();
-    // $('.footer').css('bottom', '0');
-    // $('.footer').css('left', '0');
-    // $('.footer').css('position', 'absolute');
-    // $('.footer').css('right', '0');
-});
+    var elementsToRemove = document.querySelectorAll('[id*="banner"], [class*="banner"]');
+    elementsToRemove.forEach(function(element) {
+        element.remove();
+    });
+};
--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,10 @@
-$(function(){
-    chrome.storage.sync.get('lang', function(savedLang){
-        if(!savedLang.lang){
-            savedLang.lang = 1;
-        }
-        $("#numLang").text(savedLang.lang);
-        $("select").val(savedLang.lang).prop("selected", true);
-    });
+document.addEventListener('DOMContentLoaded', () => {
+    let youtubeLink = document.querySelector('#youtube');
 
-    $('body').on('click', '#youtube', function(){
-      chrome.tabs.create({url: $(this).attr('href')});
-      return false;
-    });
-
-    $("select").on("change", function(){
-        var newLang = $(this).val();
-        chrome.storage.sync.set({'lang':newLang});
-        $("#numLang").text(newLang);
-    });
+    if (youtubeLink) {
+        youtubeLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            chrome.tabs.create({ url: event.target.getAttribute('href') });
+        });
+    }
 });
